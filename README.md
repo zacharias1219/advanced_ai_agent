6dbda0c1b88a8ca746296d9655eedf550f883fb0
Richard Abishai <rickoshade1891@gmail.com>
2024-04-23 19:11:12 +0530

    basic

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # advanced_ai_agent
+
+## Steps
+
+1. Load some data
+2. Pass the data to the LLM
+3. Take the result of one LLM and take it to another LLM
+4. Use a tool and save a file
---
+++ main.py
@@ -0,0 +1,29 @@
+from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = Ollama(model = 'mistral', request_timeout = 30.0)
+
+parser = LlamaParse(result_type = 'markdown')
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+
+emebd_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(embed_model=emebd_model, documents=documents)
+query_engine = vector_index.query_engine(llm=llm)
+
+tools = [
+    QueryEngineTool(
+        query_engine=query_engine,
+        metadata=ToolMetadata(
+            name="api_documentation",
+            description="this gives documentation about code for an API. Use this for reading docs for the API",
+        ),
+    ),
+    code_reader,
+]
