adcf2c80a70a3906725e22f6c52bbd5660462cd9
Richard Abishai <rickoshade1891@gmail.com>
2024-04-28 18:25:35 +0530

    done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemp
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from dotenv import load_dotenv
+from llama_index.core.tools import CodeReaderTool
 
 load_dotenv()
 
@@ -12,6 +13,8 @@ llm = Ollama(model = 'mistral', request_timeout = 30.0)
 parser = LlamaParse(result_type = 'markdown')
 file_extractor = {".pdf": parser}
 documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+code_reader = CodeReaderTool()
+
 
 emebd_model = resolve_embed_model("local:BAAI/bge-m3")
 vector_index = VectorStoreIndex.from_documents(embed_model=emebd_model, documents=documents)
